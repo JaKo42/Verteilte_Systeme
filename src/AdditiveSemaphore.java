37ff435879aedd25f979ce840e4ceda1d4cc724b
JaKo42 <kokkinos.Joannis@gmail.com>
2018-03-23 15:57:35 +0100

    Aufgabe 1
    ansatz mit v() und P() aufgegeben arbeite nun mit aquire und release

--- src/AdditiveSemaphore.java
+++ src/AdditiveSemaphore.java
@@ -1,5 +1,58 @@
 public class AdditiveSemaphore{
 
+    private int value;
 
+    public AdditiveSemaphore(int init){
 
+        if(init < 0)
+        {
+            throw new IllegalArgumentException("Parameter < 0");
+        }
+        this.value = init;
+    }
+
+    public synchronized void p(int x){
+
+        if(x <= 0)
+        {
+            throw new IllegalArgumentException("Parameter <= 0");
+        }
+        while(value - x < 0)
+        {
+            try{
+                wait();
+            }
+            catch(InterruptedException e)
+            {
+            }
+        }
+        value -=x;
+    }
+
+    public synchronized void v(int x){
+
+        if(x < 0)
+        {
+            throw new IllegalArgumentException("Parameter <= 0");
+        }
+        value += x;
+        notifyAll();
+    }
+
+    public void p(){
+        p(1);
+    }
+
+    public void v(){
+        v(1);
+    }
+
+    public void change(int x){
+        if (x > 0){
+            v(x);
+        }
+        else if (x < 0){
+            p(-x);
+        }
+    }
 }
--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -13,21 +13,6 @@ public class Aufgabe1 extends Thread {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public static void main(String[] args){
 
     Semaphore [] sem = new Semaphore[8];
@@ -36,12 +21,12 @@ public class Aufgabe1 extends Thread {
         sem[i] = new Semaphore(0);
         }
      new Thread_1(sem,"Thread_1");
-    /* new Thread_2(sem;"Thread_2");
-     new Thread_3(sem;"Thread_3");
-     new Thread_4(sem;"Thread_4");
-     new Thread_5(sem;"Thread_5");
-     new Thread_6(sem;"Thread_6");
-     new Thread_7(sem;"Thread_7");   */
+     new Thread_2(sem,"Thread_2");
+     new Thread_3(sem,"Thread_3");
+     new Thread_4(sem,"Thread_4");
+     new Thread_5(sem,"Thread_5");
+     new Thread_6(sem,"Thread_6");
+     new Thread_7(sem,"Thread_7");
     }
 }
 
@@ -58,16 +43,14 @@ class Thread_1 extends Thread{
 
     private void A1()
     {
-        System.out.println("Activity_4 running");
+        System.out.println("Activity_1 running");
     }
 
     public void run()
     {
         A1();
-        sem[0].v();
-        sem[1].v();
-
-
+        sem[0].release();
+        sem[1].release();
     }
 
 }
@@ -83,17 +66,21 @@ class Thread_2 extends Thread{
         start();
     }
 
-    private void A1()
+    private void A2()
     {
-        System.out.println("Activity_4 running");
+        System.out.println("Activity_2 running");
     }
 
     public void run()
     {
-        A1();
-        sem[0].v();
-        sem[1].v();
-
+        try {
+            sem[0].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        A2();
+        sem[2].release();
+        sem[3].release();
 
     }
 
@@ -110,17 +97,21 @@ class Thread_3 extends Thread{
         start();
     }
 
-    private void A1()
+    private void A3()
     {
-        System.out.println("Activity_4 running");
+        System.out.println("Activity_3 running");
     }
 
     public void run()
     {
-        A1();
-        sem[0].v();
-        sem[1].v();
-
+        try {
+            sem[1].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        A3();
+        sem[4].release();
+        sem[5].release();
 
     }
 
@@ -137,18 +128,20 @@ class Thread_4 extends Thread{
         start();
     }
 
-    private void A1()
+    private void A4()
     {
         System.out.println("Activity_4 running");
     }
 
     public void run()
     {
-        A1();
-        sem[0].v();
-        sem[1].v();
-
-
+        try {
+            sem[2].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        A4();
+        sem[6].release();
     }
 
 }
@@ -164,18 +157,25 @@ class Thread_5 extends Thread{
         start();
     }
 
-    private void A1()
+    private void A5()
     {
-        System.out.println("Activity_4 running");
+        System.out.println("Activity_5 running");
     }
 
     public void run()
     {
-        A1();
-        sem[0].v();
-        sem[1].v();
-
-
+        try {
+            sem[3].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            sem[4].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        A5();
+        sem[7].release();
     }
 
 }
@@ -191,18 +191,20 @@ class Thread_6 extends Thread{
         start();
     }
 
-    private void A1()
+    private void A6()
     {
-        System.out.println("Activity_4 running");
+        System.out.println("Activity_6 running");
     }
 
     public void run()
     {
-        A1();
-        sem[0].v();
-        sem[1].v();
-
-
+        try {
+            sem[5].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        A6();
+        sem[8].release();
     }
 
 }
@@ -225,10 +227,22 @@ class Thread_7 extends Thread{
 
     public void run()
     {
+        try {
+            sem[6].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            sem[7].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            sem[8].acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         A1();
-        sem[0].v();
-        sem[1].v();
-
 
     }
 
